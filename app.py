52fe0cb2cbf46d36b89083a1298b6ce12c182565
chengjunyi55 <43806259+chengjunyi55@users.noreply.github.com>
2018-12-02 14:07:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import os
-from flask import Flask, url_for, render_template, request
-from flask import redirect
-from flask import session
+from flask import Flask, url_for, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=9999)
