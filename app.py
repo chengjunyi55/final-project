e5e677ecdeb6f69d94b01f9e51fda0ec705bdb89
chengjunyi55 <43806259+chengjunyi55@users.noreply.github.com>
2018-12-03 01:48:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,15 @@ def render_main():
 def render_result():
     #try:
         xx1=np.array([float(request.args["gpa"])/4.00])
-        v=float(request.args["v"])
-        xx2=np.array([float(v)/170])
-        q=float(request.args["q"])
-        xx3=np.array([float(q)/170])
+        xx2=np.array([float(request.args["v"])/170])
+        xx3=np.array([float(float(request.args["q"]))/170])
         w=float(request.args["w"])
         xx4=np.array([float(w)/6.0])
         p=float(request.args["p"])
         xx5=np.array([float(p)/990])
         data=np.array([xx1,xx2,xx3,xx4,xx5]).transpose()
-        result1=float(train("accepted", data)[0][0])
-        result2=float(train("rejected", data)[0][0])
+        result1=train("accepted", data)
+        result2=train("rejected", data)
         return render_template("result.html", result1=result1, result2=result2)
     #except ValueError:
         #return "Only valid numbers can be accepted. Please try again."
