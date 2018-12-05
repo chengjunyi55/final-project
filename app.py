9719841c0675831d889d3a8d44ab623298b5df4f
chengjunyi55 <43806259+chengjunyi55@users.noreply.github.com>
2018-12-05 02:06:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,25 @@ def render_main():
 
 @app.route("/result")
 def render_result():
-    try:
-        xx1=np.array([float(request.args["gpa"])/4.00])
-        xx2=np.array([float(request.args["v"])/170])
-        xx3=np.array([float(request.args["q"])/170])
-        xx4=np.array([float(request.args["w"])/6.0])
-        xx5=np.array([float(request.args["p"])/990])
+    #try:
+        gpa=request.args.get("gpa")
+        xx1=[float(gpa)/4.00]
+        v=request.args.get("v")
+        xx2=[(float(v)-130)/40]
+        q=request.args.get("q")
+        xx3=[(float(q)-130)/40]
+        w=request.args.get("w")
+        xx4=[float(w)/6.0]
+        p=request.args.get("p")
+        xx5=[(float(p)-200)/790]
         data=np.array([xx1,xx2,xx3,xx4,xx5]).transpose()
-        result1=train("accepted", data)
-        result2=train("rejected", data)
-        return render_template("result.html", result1=result1, result2=result2)
-    except ValueError:
-        return "Only valid numbers can be accepted. Please try again."
+        result1=train(data)[0]
+        result2=train(data)[1]
+        result3=train(data)[2]
+        return render_template("result.html", result1=result1, \
+                               result2=result2, result3=result3)
+    #except ValueError:
+        #return "Only valid numbers can be accepted. Please try again."
 
 if __name__ == "__main__":
     app.run(debug=True, port=9999)
