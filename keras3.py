5e9d78f1435c53f86f745e6629cf7eadec493de2
chengjunyi55 <43806259+chengjunyi55@users.noreply.github.com>
2018-12-05 02:05:43 -0800

    Update keras3.py

--- keras3.py
+++ keras3.py
@@ -17,14 +17,16 @@ from tensorflow.python.keras.models import Model, Sequential
 from tensorflow.python.keras.layers import Input, Dense
 from tensorflow.python.keras.callbacks import Callback, EarlyStopping
 
-def train(name, data):
-    x1=[]
-    x2=[]
-    x3=[]
-    x4=[]
-    x5=[]
-    y1=[]
-    with open("thegradcafe_"+name+".json", "r") as nf:
+with open("ranking.json", "r") as rank:
+    rank_dic=json.load(rank)
+def key(no):
+    if no in range(len(rank_dic.keys())):
+        return list(rank_dic.keys())[no]
+def rank(no):
+    return int(key(no).split("-")[0])
+
+def train(data):
+    with open("thegradcafe_accepted.json", "r") as nf:
         list1=json.load(nf)
     x1=np.array(list1[0])
     x2=np.array(list1[1])
@@ -46,15 +48,63 @@ def train(name, data):
     model=Model(inputs, output)
     model.compile(optimizer='sgd', loss='mae', metrics=["mae"])
     history=model.fit(dat_train, y1_train, epochs=100, verbose=0)
-    result=round(float(model.predict(data, verbose=1)))
+    result1=round(float(model.predict(data, verbose=1)))
 
-    with open("ranking.json", "r") as rank:
-        rank_dic=json.load(rank)
-    schools=[]
-    for key in list(rank_dic.keys()):       
+    with open("thegradcafe_rejected.json", "r") as nf:
+        list1=json.load(nf)
+    x1=np.array(list1[0])
+    x2=np.array(list1[1])
+    x3=np.array(list1[2])
+    x4=np.array(list1[3])
+    x5=np.array(list1[4])
+    y1=np.array(list1[7])
+    dat=np.array([x1,x2,x3,x4,x5]).transpose()
+    idx_list=np.array(range(len(y1)))
+    idx_test=np.random.choice(len(y1), size=int(len(y1)*0.25), replace=False)
+    idx_train=np.delete(idx_list, idx_test).astype("int")
+    dat_train=dat[idx_train,:]
+    dat_test=dat[idx_test,:]
+    y1_train=y1[idx_train]
+    y1_test=y1[idx_test]
+    inputs=Input(shape=(5,))
+    y=Dense(1, activation="linear")
+    output=y(inputs)
+    model=Model(inputs, output)
+    model.compile(optimizer='sgd', loss='mae', metrics=["mae"])
+    history=model.fit(dat_train, y1_train, epochs=100, verbose=0)
+    result2=round(float(model.predict(data, verbose=1)))
+
+    schools1=[]
+    schools2=[]
+    schools3=[]
+    """
+    max_rank=100-result2
+    while len(schools2)<5:
+        if type(rank_dic.get(key(max_rank)))!="NoneType":
+            schools2.append(rank_dic.get(key(max_rank))[0])
+        max_rank=max_rank+1
+    max_rank=100-result1
+    while len(schools1)<5:
+        if type(rank_dic.get(key(max_rank)))!="NoneType":
+            schools1.append(rank_dic.get(key(max_rank))[0])
+        max_rank=max_rank+1
+    while len(schools3)<5:
+        if type(rank_dic.get(key(max_rank)))!="NoneType":
+            schools3.append(rank_dic.get(key(max_rank))[0])
+        max_rank=max_rank+1
+    """
+    for key in rank_dic.keys():
         rank=int(key.split("-")[0])
-        if result+rank==100:
-            schools.append(rank_dic.get(key)[0])
-        elif result+rank>100 and len(schools)<5:
-            schools.append(rank_dic.get(key)[0])
-    return schools
+        if result2+rank==100:
+            schools2.append(rank_dic.get(key)[0])
+        elif result2+rank>100 and len(schools2)<5:
+            schools2.append(rank_dic.get(key)[0])
+        if result1+rank==100 and rank_dic.get(key)[0] not in schools2:
+            schools1.append(rank_dic.get(key)[0])
+        elif result1+rank>100 and len(schools1)<5 and \
+             rank_dic.get(key)[0] not in schools2:
+            schools1.append(rank_dic.get(key)[0])
+        if result1+rank>100 and rank_dic.get(key)[0] not in schools1 and \
+           rank_dic.get(key)[0] not in schools2 and len(schools3)<5:
+            schools3.append(rank_dic.get(key)[0])
+    return [schools1, schools2, schools3]
